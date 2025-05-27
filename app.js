02cd019e8c5affd42071988715f9081e918ae64b
MonubiJustin <monubijustin@gmail.com>
2025-05-27 03:53:24 +0300

    added get for testing

--- app.js
+++ app.js
@@ -18,5 +18,9 @@ app.use('/api/contacts', contactRoutes);
 app.use('/api/users', userRoutes); // Add userRoutes to middleware
 app.use(error);
 
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
